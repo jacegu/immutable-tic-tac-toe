1c42a92a92e2428eda93cc96655b220f00b8e6bd
Javier Acero <j4cegu@gmail.com>
2013-06-19 10:21:27 +0200

    Fail: the game starts with an empty board

---
+++ spec/tictactoe/game_spec.rb
@@ -0,0 +1,17 @@
+describe TicTacToe::Game do
+  it 'starts with an empty board' do
+    game = described_class.new
+    expect(game).to have_an_empty_board
+  end
+
+  describe 'making a move' do
+    context 'when the move is valid' do
+      it 'knows who has the next turn'
+      it 'knows marks the board position as taken'
+    end
+
+    context 'when the move is invalid' do
+      it 'does something about it'
+    end
+  end
+end
