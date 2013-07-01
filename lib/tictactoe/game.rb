959a06eb265b6c345bcfd06d687a3857e1928504
Javier Acero <j4cegu@gmail.com>
2013-07-01 11:50:14 +0200

    Refactor: remove explicit reference to class

--- lib/tictactoe/game.rb
+++ lib/tictactoe/game.rb
@@ -26,7 +26,7 @@ module TicTacToe
 
     def make_move(position)
       return self if over? || @board.taken?(position) || !@board.valid?(position)
-      Game.new(@board.mark_position(next_turn_player, position), @players.reverse)
+      self.class.new(@board.mark_position(next_turn_player, position), @players.reverse)
     end
 
     def winner
