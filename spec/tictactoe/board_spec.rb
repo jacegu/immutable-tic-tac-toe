249510164e1076c7a217f37228496379237ffdd4
Javier Acero <j4cegu@gmail.com>
2018-03-06 09:31:56 +0100

    Update allthethings

--- spec/tictactoe/board_spec.rb
+++ spec/tictactoe/board_spec.rb
@@ -2,10 +2,10 @@ require 'board'
 
 describe TicTacToe::Board do
   it 'knows what positions are valid' do
-    expect(subject.valid?(-1)).to be_false
-    expect(subject.valid?(0)).to be_true
-    expect(subject.valid?(5)).to be_true
-    expect(subject.valid?(8)).to be_true
-    expect(subject.valid?(9)).to be_false
+    expect(subject.valid?(-1)).to eq(false)
+    expect(subject.valid?(0)).to eq(true)
+    expect(subject.valid?(5)).to eq(true)
+    expect(subject.valid?(8)).to eq(true)
+    expect(subject.valid?(9)).to eq(false)
   end
 end
